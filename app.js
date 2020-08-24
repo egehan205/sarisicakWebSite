205fefe05337e489179403730d9d737aa92e216e
egehan205 <egehankarakose25@hotmail.com>
2020-08-24 19:17:25 +0300

    added some pages

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent =
-  "";
+const homeStartingContent = "";
 const aboutContent = [
   "   Yaşıtlarına göre değerlendirildiğinde bedensel, bilişsel, sosyal, duygusal, öz bakım yönünden fark edilir biçimde yetersizliği bulunan bireylere “özel eğitime ihtiyaç duyan bireyler” denir. Bu bireylerin yetersizliklerinden en az düzeyde etkilenmesini sağlamak, bireylerin toplumla kaynaşmasına ve bağımsız olarak yaşayabilmelerine destek vermek amacıyla; bireylerin yetersizliklerine uygun ortamlarda, onlar için geliştirilmiş özel yöntemlerle sürdürülen eğitime ise “özel eğitim” denir. ",
   "   Bizde Milas Sarı Sıcak Özel Eğitim ve Rehabilitasyon Merkezi olarak 2007 yılından itibaren özel eğitime ihtiyaç duyan bireylerimize, açılış tarihinden aralıksız hizmet vermekteyiz. Merkezimiz bünyesinde fizik tedavi salonumuz, işitme engelliler bireysel-grup odamız, yaygın gelişimsel bireysel-grup odamız, zihinsel engelliler bireysel, zihinsel engelliler grup odamız, psikolojik destek odamız bulunmaktadır.",
@@ -21,11 +21,9 @@ const aboutPrograms = [
   "İşitme Engelli Bireyler Destek Eğitim Programı",
   "Bedensel Engelli Bireyler Destek Eğitim Programı",
 ];
-const contactContent =
-  "";
+const contactContent = "";
 
-const employeeContent =
-  "";
+const employeeContent = "";
 
 posts = [
   {
@@ -36,12 +34,44 @@ posts = [
   },
 ];
 
+stuffs = [];
+
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/stuffDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const stuffSchema = {
+  name: String,
+  department: String,
+};
+
+const Stuff = mongoose.model("Stuff", stuffSchema);
+
+const stuff = new Stuff({
+  name: "Egehan Karaköse",
+  department: "Developer",
+});
+//stuff.save();
+
+// Stuff.find(function (err, stuff) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     // console.log(fruits);
+//     mongoose.connection.close();
+//     stuff.forEach(function (stuff) {
+//       console.log(stuff._id);
+//     });
+//   }
+// });
 
 app.get("/", function (req, res) {
   res.render("home", {
@@ -58,9 +88,16 @@ app.get("/about", function (req, res) {
   res.render("about", { content: aboutContent, programs: aboutPrograms });
 });
 
-app.get("/employee", function (req, res) {
-  res.render("employee", { content: employeeContent });
+app
+  .route("/employee")
+  .get(function (req, res) {
+  Stuff.find({}, function (err, stuffs) {
+    res.render( "employee",{
+      foundStuff: stuffs
+    });
+  });
 });
+
 app.get("/activities", function (req, res) {
   res.render("activities", { content: aboutContent });
 });
@@ -73,6 +110,16 @@ app.get("/vision", function (req, res) {
 app.get("/service", function (req, res) {
   res.render("service", { content: aboutContent });
 });
+
+app.get("/zihinsel", function (req, res) {
+  res.render("zihinsel", { });
+});
+
+app.get("/yaygin", function (req, res) {
+  res.render("yaygin", {  });
+});
+
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
